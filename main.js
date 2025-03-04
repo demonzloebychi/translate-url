bb894c228333b6a72fcdddfc11928e84c0b5ac97
nikolas <nikolaislnkv@gmail.com>
2025-03-04 15:08:42 +0300

    create

---
+++ main.js
@@ -0,0 +1,33 @@
+const inputText = document.getElementById('inputText')
+const btn = document.getElementById('fixButton')
+const outputText = document.getElementById('outputText')
+
+
+function transliterate(text) {
+    const transliterationMap = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo',
+        'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm',
+        'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+        'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
+        'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya',
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'Yo',
+        'Ж': 'Zh', 'З': 'Z', 'И': 'I', 'Й': 'J', 'К': 'K', 'Л': 'L', 'М': 'M',
+        'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
+        'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch',
+        'Ъ': '', 'Ы': 'Y', 'Ь': '', 'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya'
+    };
+
+    return text
+        .toLowerCase()
+        .split('')
+        .map(char => transliterationMap[char] || char)
+        .join('')
+        .replace(/\s+/g, '-');
+}
+
+
+btn.addEventListener('click', function() {
+    const result = transliterate(inputText.value);
+    // console.log(result);
+    outputText.innerText = result
+})
